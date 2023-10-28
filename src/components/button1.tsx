75d4d3c202f4d109b9bb2bce4ba025fbae35ace3
Oliver Bello <obello26@icloud.com>
2023-10-28 14:38:53 -0400

    Co-authored-by: Sidrah Fawzi <sidrahf@users.noreply.github.com>

--- src/components/button1.tsx
+++ src/components/button1.tsx
@@ -1,5 +1,25 @@
-export default function Button1() {
+"use client"
+
+export default function Button1(label: string) {
+        
+    const handleClick = () => {
+        fetch("https://api.weather.gov/gridpoints/BOX/69,92/forecast")
+            .then((res) => res.json())
+            .then((json) => {
+                console.log("somethingggg");
+                console.log(json);
+            });
+    };
+
+    label = "Sidrah and Oliver";
+    
     return (
-        <button></button>
+        <button 
+            type="button" 
+            onClick={handleClick} 
+            className="p-3 px-6 border-2 border-blue-500 hover:bg-blue-500 
+            hover:text-white duration-300 rounded-full">
+            {label}
+        </button>
     );
 }
