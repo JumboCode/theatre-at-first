d44d3bfd36e7c348927ef753de1e17211733ce6c
Teddy Hwang <teddyh@Teddys-MacBook-Pro-6019.local>
2023-10-29 20:03:16 -0400

    it works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <Button2 />
         <Button3 />
         <Button4 />
-        <Button5 label="Hi Teddy!"/>
+        <Button5 label="SHOW ME THE WEATHER!!"/>
     </main>
   )
 }
--- src/components/button5.tsx
+++ src/components/button5.tsx
@@ -7,13 +7,13 @@ interface prop {
 
 
 export default function Button5(props: prop) {
-    const [properties, setWeather] = useState([]);
+    const [weather, setWeather] = useState<{name: string, shortForecast: string} | null>(null);
 
     const getWeather = () => {
         fetch(`https://api.weather.gov/gridpoints/BOX/69,92/forecast`)
         .then(res => res.json())
         .then(res => {
-            setWeather(res.properties)
+            setWeather(res.properties.periods[0])
         }); 
     };
     
@@ -21,12 +21,12 @@ export default function Button5(props: prop) {
         <div>
             <button onClick={getWeather} className="bg-red-700 border-[6px] border-red-900  text-white px-12 py-4 rounded-full text-3xl font-bold">{props.label}</button>
 
-            {properties.map((now) => (
-                <div key={now.periods[0].name}>
-                <p>{now.periods[0].name}</p>
-                <i>{now.periods[0].shortForecast}</i>
-                </div>
-            ))}
+            { weather != null ?
+                <div>
+                    <p>{weather.name}</p>
+                    <i>{weather.shortForecast}</i>
+                </div> : <div></div>
+            }
         </div>
     );
 }
