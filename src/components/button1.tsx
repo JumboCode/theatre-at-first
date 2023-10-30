66125a511453a5fff708a9e9607de264cdaba4b2
Oliver Bello <obello26@icloud.com>
2023-10-29 20:05:38 -0400

    button done
    Co-authored-by: Sidrah Fawzi <sidrahf@users.noreply.github.com>

--- src/components/button1.tsx
+++ src/components/button1.tsx
@@ -1,25 +1,42 @@
 "use client"
+import { useState } from "react";
 
 export default function Button1(label: string) {
         
+    
+    label = "Sidrah and Oliver";
+
+    const [temp, setTemp] = useState();
+    const [tempUnit, setTempUnit] = useState();
+    const [shortForecast, setShortForecast] = useState();
+    
     const handleClick = () => {
         fetch("https://api.weather.gov/gridpoints/BOX/69,92/forecast")
             .then((res) => res.json())
             .then((json) => {
-                console.log("somethingggg");
-                console.log(json);
+                setTemp(json.properties.periods[0].temperature);
+                setTempUnit(json.properties.periods[0].temperatureUnit);
+                setShortForecast(json.properties.periods[0].shortForecast);
             });
     };
 
-    label = "Sidrah and Oliver";
-    
+   
     return (
-        <button 
-            type="button" 
-            onClick={handleClick} 
-            className="p-3 px-6 border-2 border-blue-500 hover:bg-blue-500 
-            hover:text-white duration-300 rounded-full">
-            {label}
-        </button>
+        <div className="flex flex-col gap-4 justify-center">
+            <button 
+                type="button" 
+                onClick={handleClick} 
+                className="p-3 px-6 border-2 border-blue-500 hover:bg-blue-500 
+                 hover:text-white duration-300 rounded-full">
+                {label}
+            </button>
+            <p>
+                Current temperature: {temp} {tempUnit} 
+            </p>
+            <p>
+                {shortForecast}
+            </p>
+        </div>
+        
     );
 }
