b286e250ed312e262d932053ba8ab353890a2967
Dan <daniel.glorioso@tufts.edu>
2023-10-29 20:36:20 -0400

    Co-authored-by: ShepardRodgers <ShepardRodgers@users.noreply.github.com>

--- src/components/button3.tsx
+++ src/components/button3.tsx
@@ -1,5 +1,25 @@
-export default function Button3() {
+"use client"
+import { useState } from "react";
+
+export default function Button3(props) {
+    const [forecast, setForecast] = useState<{name: string, shortForecast: string}>(
+        {name: "Text A", shortForecast: "Text B"}
+    );
+
+    function handleClick() {
+        fetch("https://api.weather.gov/gridpoints/BOX/69,92/forecast")
+            .then((res) => res.json())
+            .then((json) => {
+                setForecast(json.properties.periods[0]);
+        });
+
+    };
+
     return (
-        <button></button>
+        <>
+            <button onClick={handleClick}>{props.name}</button>
+            <p>{forecast.name}</p>
+            <i>{forecast.shortForecast}</i>
+        </>
     );
 }
