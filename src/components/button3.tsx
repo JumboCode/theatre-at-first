e5a2f117b0860c47fa47e9897bc805a84a6e08b8
Dan <daniel.glorioso@tufts.edu>
2023-11-03 15:35:17 -0400

    Co-authored-by: Shepard Rodgers <ShepardRodgers@users.noreply.github.com>

--- src/components/button3.tsx
+++ src/components/button3.tsx
@@ -2,9 +2,7 @@
 import { useState } from "react";
 
 export default function Button3(props) {
-    const [forecast, setForecast] = useState<{name: string, shortForecast: string}>(
-        {name: "Text A", shortForecast: "Text B"}
-    );
+    const [forecast, setForecast] = useState<{name: string, shortForecast: string}|undefined>(undefined);
 
     function handleClick() {
         fetch("https://api.weather.gov/gridpoints/BOX/69,92/forecast")
@@ -17,12 +15,13 @@ export default function Button3(props) {
 
     return (
         <>
-            <button onClick={handleClick}>{props.name}</button>
-            <p>{forecast.name}</p>
-            <i>{forecast.shortForecast}</i>
+            <button onClick={handleClick} className="bg-blue-900 hover:bg-red-700 text-white font-bold py-2 px-4 rounded border-8 border-emerald-800">{props.name}</button>
+            { forecast && (
+                <div>
+                    <i className="text-center font-mono text-base">Weather {forecast.name}:</i>
+                    <p className="text-center font-mono text-xl">{forecast.shortForecast}</p>
+                </div>
+            )}
         </>
     );
 }
-
-
-// fetch test
