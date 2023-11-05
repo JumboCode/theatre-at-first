4b9d472a341b3149d22d024d055274ce3b16e116
Teddy Hwang <teddyh@Teddys-MacBook-Pro-6019.local>
2023-11-05 15:38:58 -0500

    Fixed button to handle null val

--- src/components/button5.tsx
+++ src/components/button5.tsx
@@ -7,13 +7,16 @@ interface prop {
 
 
 export default function Button5(props: prop) {
-    const [weather, setWeather] = useState<{probabilityOfPrecipitation: string[]} | null>(null);
+    const [weather, setWeather] = useState<{probabilityOfPrecipitation:[]} | null>(null);
 
     const getWeather = () => {
-        fetch(`https://api.weather.gov/gridpoints/BOX/69,92/forecast`)
+        fetch(`https://api.weather.gov/gridpoints/BOX/71,90/forecast`)
         .then(res => res.json())
         .then(res => {
-            setWeather(res.properties.periods[0])
+
+        const probability = res.properties.periods[0].probabilityOfPrecipitation.value;
+        console.log(probability);
+        setWeather({ probabilityOfPrecipitation: probability !== null ? probability : 0 });
         }); 
     };
     
@@ -22,13 +25,13 @@ export default function Button5(props: prop) {
             <button onClick={getWeather} className="bg-red-700 border-[6px] border-red-900  text-white px-12 py-4 rounded-full text-3xl font-bold">{props.label}</button>
 
             {weather ? (
-                weather.probabilityOfPrecipitation.value > 55 ? (
+                weather.probabilityOfPrecipitation > 55 ? (
                     <div>
-                        <p className="text-center font-bold">Dream harder. We are in New England. {weather.probabilityOfPrecipitation.value}% chance of rain today.</p>
+                        <p className="text-center font-bold">Dream harder. We are in New England. {weather.probabilityOfPrecipitation}% chance of rain today.</p>
                     </div>
                 ) : (
                     <div>
-                        <p className="text-center font-bold">Your wish has been granted. {weather.probabilityOfPrecipitation.value}% chance of rain today.</p>
+                        <p className="text-center font-bold">Your wish has been granted. {weather.probabilityOfPrecipitation}% chance of rain today.</p>
                     </div>
                 )
             ) : (
