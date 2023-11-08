332adf624831ebc5fc2a2fe872f5632f0a8a289e
brandondionisio <brandon.dionisio@tufts.edu>
2023-11-08 16:22:40 -0500

    Worked on Single Item Detail
    Co-authored-by: o.b. bello <bell-oh@users.noreply.github.com>

--- src/app/item-detail-test/page.tsx
+++ src/app/item-detail-test/page.tsx
@@ -1,8 +1,39 @@
 
-export default function itemDetailTest() {
+interface itemDetailProps {
+  name: string,
+  tags: string[],
+  description: string,
+}
+
+export default function itemDetailTest(name: string, tags: string[], description: string) {
+
+  name = "Item Name";
+  tags = ["tag1", "longtag2", "reallyreallylongtag3", "tag4"];
+  description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        Hi
+    <main className="">
+      <div className="flex flex-col gap-10 items-left p-20 m-10 bg-white w-[50%]">
+        <div className="text-4xl font-bold">
+          {name}
+        </div>
+        <div className="flex flex-row gap-6 overflow-scroll">
+          {tags.map((tag) => (
+            <div key={tag} className="border-2 border-black rounded-lg px-6 py-2 bg-gray-50">
+              <p>{tag}</p>
+            </div>
+          ))}
+        </div>
+        <div className="bg-black w-[100%] h-px border-2 border-black">
+          
+        </div>
+        <div>
+          <div className="text-xl font-bold mb-5">
+            Description
+          </div>
+          {description}
+        </div>
+      </div>
     </main>
   )
 }
