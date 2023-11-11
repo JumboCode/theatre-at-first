e0830163330011dd3af22736facd212efdce819f
Ella Guo <yixin.guo@tufts.edu>
2023-11-11 01:56:16 -0500

    updated resizeableimg

--- src/app/resizeableSandbox/page.tsx
+++ src/app/resizeableSandbox/page.tsx
@@ -1,9 +1,63 @@
+"use client";
+import React, { useState } from "react";
 import ResizeableIMG from "../../components/resizeableIMG";
+import CuteDog1 from "../../../public/images/cute_dog1.jpg";
+import CuteDog2 from "../../../public/images/cute_dog2.jpg";
+import CuteDog3 from "../../../public/images/cute_dog3.jpg";
+import CuteDog4 from "../../../public/images/cute_dog4.jpg";
+import CuteDog5 from "../../../public/images/cute_dog5.jpg";
+
+const imageList = [CuteDog1, CuteDog2, CuteDog3, CuteDog4, CuteDog5];
 
 export default function Home() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageList.length);
+  };
+
+  const prevImage = () => {
+    setCurrentImageIndex(
+      (prevIndex) => (prevIndex - 1 + imageList.length) % imageList.length
+    );
+  };
+  //   return (
+  //     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  //       {imageList.map((v) => (
+  //         <ResizeableIMG
+  //           width={600}
+  //           height={400}
+  //           src={v.src}
+  //           alt="pics of cookies"
+  //         />
+  //       ))}
+  //     </main>
+  //   );
+  // }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <ResizeableIMG />
+      <div className="image-slider" style={{ position: "relative" }}>
+        <button
+          className="slider-button"
+          onClick={prevImage}
+          style={{ left: 0, top: "50%", transform: "translateY(-50%)" }}
+        >
+          Prev
+        </button>
+        <ResizeableIMG
+          width={600}
+          height={400}
+          src={imageList[currentImageIndex].src}
+          alt="pics of cookies"
+        />
+        <button
+          className="slider-button"
+          onClick={prevImage}
+          style={{ left: 0, top: "50%", transform: "translateY(-50%)" }}
+        >
+          Next
+        </button>
+      </div>
     </main>
-  )
+  );
 }
--- src/components/resizeableIMG.tsx
+++ src/components/resizeableIMG.tsx
@@ -1,10 +1,19 @@
-"use client";
+import Image from "next/image";
 
-export default function ResizeableIMG() {
-
-    return (
-        <div>
-            <p>Hello</p>
-        </div>
-    );
+export default function ResizeableIMG(
+  props: React.ComponentProps<typeof Image>
+) {
+  return (
+    <div>
+      <Image
+        {...props}
+        style={{
+          width: `${props.width}px`,
+          height: `${props.height}px`,
+          overflow: "hidden",
+          objectFit: "cover",
+        }}
+      />
+    </div>
+  );
 }
