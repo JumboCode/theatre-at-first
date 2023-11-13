5499cfafd4179cc6f23d27eb3cb4e2a374007ac7
Oliver Bello <obello26@icloud.com>
2023-11-12 20:40:41 -0500

    added current status and made tags wrap

--- src/app/item-detail-test/page.tsx
+++ src/app/item-detail-test/page.tsx
@@ -1,8 +1,13 @@
-export default function itemDetailTest(name: string, tags: string[], description: string) {
+export default function itemDetailTest(name: string, tags: string[], description: string, status: string) {
+
+    // make request to API to get list of updates
+    const updates: string[] = ["Update One", "Update Two", "Update Three"]; 
 
   name = "Item Name";
   tags = ["tag1", "longtag2", "reallyreallylongtag3", "tag4"];
   description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  status = "Most Recent";
+
 
   return (
     <main className="">
@@ -10,7 +15,10 @@ export default function itemDetailTest(name: string, tags: string[], description
         <div className="text-4xl font-bold">
           {name}
         </div>
-        <div className="flex flex-row gap-6 overflow-scroll">
+        <div>
+            {status}
+        </div>
+        <div className="flex flex-row gap-6 flex-wrap">
           {tags.map((tag) => (
             <div key={tag} className="border-2 border-black rounded-lg px-6 py-2 bg-gray-50">
               <p>{tag}</p>
