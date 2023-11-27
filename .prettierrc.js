5a64c19d9cb463a5fd147442f110c6f7e30a2c14
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-11-26 22:47:52 -0500

    update prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+    trailingComma: "es5",
+    useTabs: false,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    bracketSameLine: false,
+};
+
+module.exports = config;
