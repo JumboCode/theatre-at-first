37fa6f6612af5ee63f98ee29e3e692f03ba24bfa
Nehir Ozden <nehirozden@Nehirs-MacBook-Pro.local>
2023-12-03 19:59:17 -0500

    worked on dropdown some more

--- src/app/tag-dropdown-test/page.tsx
+++ src/app/tag-dropdown-test/page.tsx
@@ -2,12 +2,12 @@ import TagDropdown from "@/components/TagDropdown";
 
 export default function testPage() {
 
-    const allTags = ["one", "two", "three", "four", "five"];
+    const allTags = ["one", "two", "three", "four", "five", "six", "seven"];
     
     return (
         <main>
             <div className="bg-purple-400 flex flex-col justify-center border-2 border-blue-500">
-                <div className="w-[800px] border-2 border-black">
+                <div className="w-[588px] border-2 border-black">
                     <TagDropdown tags={allTags}></TagDropdown>
                 </div>
             </div> 
--- src/components/TagDropdown.tsx
+++ src/components/TagDropdown.tsx
@@ -12,12 +12,16 @@ import { useState } from "react";
 //Once you click on a tag, it should be added to the product's description or
 //smth
 
+//We either create a tag or pick one that already exsits 
+
 // Need to make GET request to Database to retrieve the tags array 
 //When the tags are selected, they should be sent back to the schema to be stored?
 // The tags that are selected (q: what is the general list of tags? Hard coding 
 //for now)get added to the item's "tags" array 
 
 //Prop is an array of all the tags possible to be added to an item 
+
+//scrolly bar is gone 
 interface TagDropdownProps {
     tags: string[]
 }
@@ -37,13 +41,19 @@ export default function TagDropdown(
     
     return (
         <div className="flex flex-col p-4 gap-4">
-            <div onClick={handleClick} className="border-2 h-12">
-                Dropdown bar
+            <div onClick={handleClick} className="inline-block align-middle pl-2 border-2 rounded-lg h-[46px]">
+                <p className="inline-block align-middle border">
+                    Dropdown bar
+                </p>
             </div>
             {
                 display && (
-                    <div className="border-2 h-24">
-                    Container of tags list 
+                    <div className="border-2 h-[144px] rounded-xl overflow-auto">
+                        {props.tags.map((tag) => (
+                            <div key={tag} className="pl-2 hover:bg-violet-600 hover:font-bold active:bg-violet-700 py-2">
+                                <p>{tag}</p>
+                            </div>
+                        ))} 
                     </div>
                 )
             }
