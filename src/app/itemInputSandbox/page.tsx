f3e882d3935e96bc0cb65d06e4d6066e0e882201
Shepard Rodgers <shepardrodgers@Shepards-MacBook-Air.local>
2023-12-05 20:53:00 -0500

    input form finished

--- src/app/itemInputSandbox/page.tsx
+++ src/app/itemInputSandbox/page.tsx
@@ -2,7 +2,7 @@ import ItemInput from "../../components/itemInputForm";
 
 export default function Preview() {
     return (
-        <main className="h-screen flex items-center"> 
+        <main className="h-screen flex items-center justify-center space-x-4"> 
             <ItemInput/>
         </main>
     );
--- src/components/itemInputForm.tsx
+++ src/components/itemInputForm.tsx
@@ -1,36 +1,71 @@
 "use client";
+import { useState } from 'react';
 
 interface Prop {
     
 }
 
 export default function ItemInput(props: Prop) {
+  const [productName, setProductName] = useState("");
+  const [description, setDescription] = useState("");
+  const [isBold, setIsBold] = useState(false);
+  const [isItalic, setIsItalic] = useState(false);
+  const [isUnderlined, setIsUnderlined] = useState(false);
+
+  const handleBoldClick = (e) => {
+    e.preventDefault();
+    setIsBold(!isBold);
+  };
+
+  const handleItalicClick = (e) => {
+    e.preventDefault();
+    setIsItalic(!isItalic);
+  };
+
+  const handleUnderlineClick = (e) => {
+    e.preventDefault();
+    setIsUnderlined(!isUnderlined);
+  };
+
     return (
-        <>
-          <label>
-            Product Name <input name="ProductName" />
-          </label>
-          <hr/>
-          <label>
-            Product Description <input type="checkbox" name="myCheckbox" />
-          </label>
-          <hr/>
-          <p>
-            Radio buttons:
-            <label>
-              <input type="radio" name="myRadio" value="option1" />
-              Option 1
-            </label>
-            <label>
-              <input type="radio" name="myRadio" value="option2" />
-              Option 2
-            </label>
-            <label>
-              <input type="radio" name="myRadio" value="option3" />
-              Option 3
-            </label>
-          </p>
-          <hr/>
-        </>
+        <div className="outer-box bg-white rounded-xl">
+            <form className="m-5">
+                <div className="text-gray-400 font-bold">Product Name</div>
+                <input className="text-gray-950 rounded border-2" style={{width: "450px"}} 
+                        value={productName} onChange={(e) => setProductName(e.target.value)} />
+                <div className="text-gray-400 font-bold">Product Description</div>
+                <div className="text-gray-950">
+                    <button onClick={handleBoldClick} 
+                            style={{ fontWeight: isBold ? 'bold' : 'normal', marginRight: '8px',}}>
+                    B
+                    </button>
+                    <button onClick={handleItalicClick} 
+                            style={{ fontStyle: isItalic ? 'italic' : 'normal' , marginRight: '8px',}}>
+                    I
+                    </button>
+                    <button onClick={handleUnderlineClick} 
+                            style={{ textDecoration: isUnderlined ? 'underline' : 'none' , marginRight: '8px',}}>
+                    U
+                    </button>
+                </div>
+
+                <textarea className="text-gray-950 rounded rounded border-2" 
+                          value={description} 
+                          onChange={(e) => setDescription(e.target.value)}
+                          style={{
+                              height:"200px",
+                              width: "450px",
+                              fontWeight: isBold ? 'bold' : 'normal',
+                              fontStyle: isItalic ? 'italic' : 'normal',
+                              textDecoration: isUnderlined ? 'underline' : 'none',
+                          }}/>
+                <div className="text-gray-950">
+                  <h2>Entered Values</h2>
+                  <p>Product Name: {productName}</p>
+                  <p>Product Description: {description}</p>
+                </div>
+            </form>
+        </div>
       );
 }
+
