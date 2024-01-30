c4d82f3e4abbbbb865f232cd766212dd9c292d2d
liam-strand <Liam.Strand@tufts.edu>
2024-01-29 21:32:16 -0500

    finish first draft of item apis
    Co-authored-by: Amitav Nott <58123848+anott03@users.noreply.github.com>

---
+++ src/app/item/route.ts
@@ -0,0 +1,78 @@
+import { InsertItem, items } from "@/db/schema";
+import db from "@/db/drizzle";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    const data = await req.json();
+    console.log(data);
+    let name: string = data.name;
+    let desc: string = data.desc;
+    let tags: string[] = data.tags;
+    let imageUrl: string = data.imageUrl;
+    let status: string = data.status;
+
+    //check for missing fields
+    if (
+        name == undefined ||
+        desc == undefined ||
+        tags == undefined ||
+        imageUrl == undefined ||
+        status == undefined
+    ) {
+        return NextResponse.json(
+            {
+                message: "Missing fields!",
+            },
+            {
+                status: 400,
+            }
+        );
+    } else {
+        let result = await db.insert(items).values({
+            name,
+            desc,
+            tags,
+            imageUrl,
+            status,
+        })
+        .returning();
+        return NextResponse.json(
+            {
+                message: `Item added to the database: ${result.toString()}`,
+            },
+            {
+                status: 200,
+            }
+        );
+    }
+}
+
+export async function GET(req: Request) {
+        const req_data = await req.json();
+        const id = req_data.item_id;
+        const result = await db.query.items.findFirst({
+                with: {
+                        id: id
+                }
+        });
+
+        if (result) {
+                return NextResponse.json(
+                        {
+                                message: result
+                        },
+                        {
+                                status: 200
+                        }
+                )
+        } else {
+                return NextResponse.json(
+                        {
+                                message: `item ${id} is not found in the database`,
+                        },
+                        {
+                                status: 400,
+                        }
+                )
+        }
+}
