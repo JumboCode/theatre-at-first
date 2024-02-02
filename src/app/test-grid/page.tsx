e684288309408cc313dcd54dabaf4724b028203e
Teddy Hwang <teddyh@Teddys-MacBook-Pro-7327.local>
2024-02-02 18:08:38 -0500

    grid view is doneeeeee

--- src/app/test-grid/page.tsx
+++ src/app/test-grid/page.tsx
@@ -3,16 +3,19 @@ import Grid from "./../../components/grid";
 
 
 
-// Render the Home component somewhere in your app with the array of components
 export default function Home() {
-        // Create an array of Item components or any other components
+        // Store components in an array to use in <Grid/>
+        // Grid can be found in components folder
         const componentsArray = [
                 <Item title="Title 1" status="Status 1" image="image1.png"/>,
                 <Item title="Title 2" status="Status 2" image="image2.png"/>,
-                // Add more components as needed
+                <Item title="Title 3" status="Status 3" image="image3.png"/>,
+                <Item title="Title 4" status="Status 4" image="image4.png"/>,
         ];
     
         return (
+        <div className="h-screen">
                 <Grid components={componentsArray} />
+        </div>
         );
 }
--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -4,12 +4,11 @@ import React from "react";
 
 export default function Grid({ components }) { // Receive components (elements) as a prop
     return (
-        <main className="grid-container">
+        <div className="grid-container flex flex-row gap-4 m-8 h-3/4">
             {components.map((element, index) => (
                 // Directly render the element without wrapping it in a JSX tag
-                // The key should have been provided already on the elements
                 React.cloneElement(element, { key: index })
             ))}
-        </main>          
+        </div>          
     );
 }
