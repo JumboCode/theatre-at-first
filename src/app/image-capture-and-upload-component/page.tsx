ead088c60a46e15eebaca9c14712d78997d5a565
liam-strand <Liam.Strand@tufts.edu>
2024-03-25 15:46:19 -0400

    use react-webcam instead hopefully this will help

--- src/app/image-capture-and-upload-component/page.tsx
+++ src/app/image-capture-and-upload-component/page.tsx
@@ -1,9 +1,20 @@
+"use client";
+
 import ImageCapture from "../../components/image-capture-and-upload-component";
 
 export default function Home() {
     return (
         <main className="justify-between p-24">
-            <ImageCapture />
+            <ImageCapture
+                imageCallback={(imageBlob) => {
+                    const url = window.URL.createObjectURL(imageBlob);
+                    const link = document.createElement('a');
+                    link.href = url
+                    link.download = "image.jpeg"
+                    link.click()
+                    window.URL.revokeObjectURL(url);
+                }}
+            />
         </main>
     );
 }
--- src/components/image-capture-and-upload-component.tsx
+++ src/components/image-capture-and-upload-component.tsx
@@ -1,119 +1,34 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useRef, useCallback } from "react";
+import Webcam from "react-webcam";
 
-export default function ImageCapture() {
-  (function() {
-    // The width and height of the captured photo. We will set the
-    // width to the value defined here, but the height will be
-    // calculated based on the aspect ratio of the input stream.
-  
-    var width = 700;    // We will scale the photo width to this
-    var height = 0;     // This will be computed based on the input stream
-  
-    // |streaming| indicates whether or not we're currently streaming
-    // video from the camera. Obviously, we start at false.
-  
-    var streaming = false;
-  
-    // The various HTML elements we need to configure or control. These
-    // will be set by the startup() function.
-  
-    let video: HTMLVideoElement;
-    let canvas: HTMLCanvasElement;
-    let photo: HTMLImageElement;
-    let startbutton;
+interface ImageCaptureProps {
+    imageCallback: (imageBlob: Blob) => void;
+}
 
-    function startup() {
-      video = document.getElementById('video') as HTMLVideoElement;
-      canvas = document.getElementById('canvas') as HTMLCanvasElement;
-      photo = document.getElementById('photo') as HTMLImageElement;
-      startbutton = document.getElementById('startbutton');
-  
-      if (video) {
-        navigator.mediaDevices.getUserMedia({video: true, audio: false})
-        .then(function(stream) {
-          video.srcObject = stream;
-          video.play();
-        })
-        .catch(function(err) {
-          console.log("An error occurred: " + err);
-        });
-  
-      video.addEventListener('canplay', function(ev){
-        if (!streaming) {
-          height = video.videoHeight / (video.videoWidth/width);
-        
-          // Firefox currently has a bug where the height can't be read from
-          // the video, so we will make assumptions if this happens.
-        
-          if (isNaN(height)) {
-            height = width / (4/3);
-          }
-        
-          video.setAttribute('width', width.toString());
-          video.setAttribute('height', height.toString());
-          canvas.setAttribute('width', width.toString());
-          canvas.setAttribute('height', height.toString());
-          streaming = true;
-        }
-      }, false);
-  
-      startbutton?.addEventListener('click', function(ev){
-        takepicture();
-        ev.preventDefault();
-      }, false);
-      
-      clearphoto();
-      }
-    }
-  
-    // Fill the photo with an indication that none has been
-    // captured.
-  
-    function clearphoto() {
-      if (video !== null && canvas !== null && photo !== null) {
-        var context = canvas.getContext('2d');
-        if (context) {
-          context.fillStyle = "#AAA";
-          context.fillRect(0, 0, canvas.width, canvas.height);
-        }
-    
-        var data = canvas.toDataURL('image/png');
-        photo.setAttribute('src', data);
-      }
-    }
-    
-    // Capture a photo by fetching the current contents of the video
-    // and drawing it into a canvas, then converting that to a PNG
-    // format data URL. By drawing it on an offscreen canvas and then
-    // drawing that to the screen, we can change its size and/or apply
-    // other changes before drawing it.
-  
-    function takepicture() {
-      if (video !== null && canvas !== null && photo !== null) {
-        var context = canvas.getContext('2d');
-        if (width && height) {
-          canvas.width = width;
-          canvas.height = height;
-          context?.drawImage(video, 0, 0, width, height);
-        
-          var data = canvas.toDataURL('image/png');
-          photo.setAttribute('src', data);
-        } else {
-          clearphoto();
-        }
-      }
-    }
-  
-    // Set up our event listener to run the startup process
-    // once loading is complete.
-    window.addEventListener('load', startup, false);
-  })();
+export default function ImageCapture({ imageCallback }: ImageCaptureProps) {
+    const videoConstraints = {
+        width: 720,
+        height: 720,
+        facingMode: "environment",
+    };
 
-  return (
-    <div className="camera">
-    <video id="video">Video stream not available.</video>
-    <button id="startbutton">Take photo</button>
-  </div>
-  )
+    const webcamRef = useRef(null);
+    const capture = useCallback(() => {
+        const imageData = webcamRef.current.getScreenshot();
+        imageCallback(new Blob([imageData], { type: "image/jpeg"}));
+    }, [webcamRef, imageCallback]);
+    return (
+        <div>
+            <Webcam
+                audio={false}
+                ref={webcamRef}
+                height={720}
+                screenshotFormat="image/jpeg"
+                width={720}
+                videoConstraints={videoConstraints}
+            ></Webcam>
+            <button onClick={capture}>Capture photo</button>
+        </div>
+    );
 }
