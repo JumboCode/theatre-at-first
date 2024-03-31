d70ce12d5b4a83db455972e37e9f3312c8e2e4f6
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2024-03-31 19:56:35 -0400

    Co-authored-by: Nehir Ozden <nehirozden@users.noreply.github.com>

--- src/components/user-summary.tsx
+++ src/components/user-summary.tsx
@@ -10,33 +10,37 @@ interface UserProps {
     firstname: string;
     lastname: string;
     access: string;
-    image: StaticImageData;
+    // image: StaticImageData;
 }
 
 export default function UserSummary(props: UserProps) {
     return (
-        <div className="flex flex-row items-center gap-10 p-5 bg-white w-[35%] text-black">
-            <ResizeableIMG
-                width={125}
-                height={125}
+        <div className="flex flex-row items-center gap-4 p-5 text-black">
+            {/* <ResizeableIMG
+                width={50}
+                height={50}
                 className="
                 rounded-full ...
-                border-4
+                border-2
                 w-1/2
                 my-4
                 md: w-4/12
-                border-white
+                border-amber-600
                 drop-shadow-lg
                 ring-1
-                ring-gray-400	
+                ring-amber-500	
                 backdrop-blur-lg
                 outline-[8px]
                 "
                 src={props.image.src}
-            />
+            /> */}
             <div>
-                <p className="text-3xl font-bold max-w-3 line-clamp-1 truncate text-overflow: ellipsis;">{props.firstname} {props.lastname}</p>
-                <p className="text-2xl text-gray-400 max-w-3 line-clamp-1 text-overflow: ellipsis;">{props.access}</p>
+                <p className="text-18px text-amber-500 font-bold max-w-3 line-clamp-1 truncate text-overflow: ellipsis;">
+                    {props.firstname} {props.lastname}
+                </p>
+                <p className="text-16px text-stone-400 max-w-3 line-clamp-1 text-overflow: ellipsis;">
+                    {props.access}
+                </p>
             </div>
         </div>
     );
