9e52b96c9dbf2effd8d34ee3d03356824d5aaf2a
brandondionisio <brandon.dionisio@tufts.edu>
2024-04-02 15:10:39 -0400

    finished ticket

--- src/components/image-capture-and-upload-component.tsx
+++ src/components/image-capture-and-upload-component.tsx
@@ -16,10 +16,9 @@ export default function ImageCapture({ imageCallback }: ImageCaptureProps) {
 
     const webcamRef: MutableRefObject<Webcam | null> = useRef(null);
     const capture = useCallback(() => {
-
         // imageData is base64 encoded jpeg
         const imageData = webcamRef.current?.getScreenshot()!;
-        const imgBlob = new Blob([imageData], {type: "image/jpeg"});
+        const imgBlob = new Blob([imageData], { type: "image/jpeg" });
         imageCallback(imgBlob);
     }, [webcamRef, imageCallback]);
     return (
@@ -32,7 +31,10 @@ export default function ImageCapture({ imageCallback }: ImageCaptureProps) {
                 width={1240}
                 videoConstraints={videoConstraints}
             ></Webcam>
-            <button onClick={capture} className="mt-2 py-3 pr-3 pl-3 rounded-3xl text-gray-600 bg-gray-100 border-4 border-amber-700 hover:bg-gray-600 hover:text-gray-100">
+            <button
+                onClick={capture}
+                className="mt-2 py-3 pr-3 pl-3 rounded-3xl text-gray-600 bg-gray-100 border-4 border-amber-700 hover:bg-gray-600 hover:text-gray-100"
+            >
                 <Camera></Camera>
             </button>
         </div>
--- src/components/itemUploadPage.tsx
+++ src/components/itemUploadPage.tsx
@@ -8,16 +8,14 @@ import { Image } from "./Button-Graphics";
 
 import ImageCapture from "./image-capture-and-upload-component";
 
- // import "@/components/ImageCapture"
+// import "@/components/ImageCapture"
 
 interface UploadProps {
     tags: string[];
     show_camera: boolean;
-    
 }
 
 export default function ItemUpload(props: UploadProps) {
-
     const [productName, setProductName] = useState("");
     const [description, setDescription] = useState("");
     const [selectedTags, setSelectedTags] = useState<string[]>([]);
@@ -30,7 +28,7 @@ export default function ItemUpload(props: UploadProps) {
 
     const handleImageFile = () => {
         props.show_camera = true;
-    }
+    };
 
     // const handleChangeDescription = (desc: string) => {
     //     setDescription(desc);
@@ -41,7 +39,7 @@ export default function ItemUpload(props: UploadProps) {
         setProductName("");
         setDescription("");
         setSelectedTags([]);
-    }
+    };
 
     const handleAdd = async () => {
         //send stored info to database (using "add" api route)
@@ -51,19 +49,19 @@ export default function ItemUpload(props: UploadProps) {
             desc: description,
             tags: selectedTags,
             imageUrl: "",
-            status: "active"
+            status: "active",
         };
 
         try {
             // Send the item object to the database using API
-            const response = await fetch('/item', {
-                method: 'POST',
+            const response = await fetch("/item", {
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json'
+                    "Content-Type": "application/json",
                 },
-                body: JSON.stringify(newItem)
+                body: JSON.stringify(newItem),
             });
-    
+
             if (response.ok) {
                 // Clear the components if the item was successfully added
                 setProductName("");
@@ -71,12 +69,12 @@ export default function ItemUpload(props: UploadProps) {
                 setSelectedTags([]);
             } else {
                 // Handle error scenarios
-                console.error('Failed to add item:', response.statusText);
+                console.error("Failed to add item:", response.statusText);
             }
         } catch (error) {
-            console.error('Error adding item:', error);
+            console.error("Error adding item:", error);
         }
-    }
+    };
 
     return (
         <div className="bg-white h-24 overflow-auto overscroll-y-auto overscroll-x-auto min-h-screen">
@@ -94,21 +92,27 @@ export default function ItemUpload(props: UploadProps) {
                         </div>
                     </div>
                 </div>
-            <div className="py-8 last:pb-0 flex w-full text-neutral-700 font-extrabold">
-                Description
-                <div className="ml-[550px]">
-                Category
+                <div className="py-8 last:pb-0 flex w-full text-neutral-700 font-extrabold">
+                    Description
+                    <div className="ml-[550px]">Category</div>
                 </div>
             </div>
-        </div>
-            
 
             <div className="flex w-full mt-2 ml-20 mr-20 h-[370px]">
                 <div className="border rounded-xl border-solid border-amber-500 w-[600px] bg-orange-50 shadow-xl">
-                    <ItemInput productName={productName} setProductName={setProductName} description={description} setDescription={setDescription}></ItemInput>
+                    <ItemInput
+                        productName={productName}
+                        setProductName={setProductName}
+                        description={description}
+                        setDescription={setDescription}
+                    ></ItemInput>
                 </div>
                 <div className="ml-10 mt-6 border rounded-xl border-solid border-amber-500 w-[600px] bg-orange-50 h-[320px] shadow-xl">
-                    <TagDropdown tags={props.tags} selectedTags={selectedTags} setSelectedTags={setSelectedTags}></TagDropdown>
+                    <TagDropdown
+                        tags={props.tags}
+                        selectedTags={selectedTags}
+                        setSelectedTags={setSelectedTags}
+                    ></TagDropdown>
                 </div>
             </div>
 
@@ -117,43 +121,45 @@ export default function ItemUpload(props: UploadProps) {
             </div>
 
             <div className="flex justify-start ml-20">
-                { props.show_camera ? (<ImageCapture imageCallback={(blob) => {}}></ImageCapture>) : 
-                    (<button className="border-dashed border-2 font-bold w-[1240px] h-[500px] bg-orange-50 rounded-2xl border-amber-500 shadow-xl flex flex-col justify-center items-center"
-                        onClick={handleImageFile}>
+                {props.show_camera ? (
+                    <ImageCapture imageCallback={(blob) => {}}></ImageCapture>
+                ) : (
+                    <button
+                        className="border-dashed border-2 font-bold w-[1240px] h-[500px] bg-orange-50 rounded-2xl border-amber-500 shadow-xl flex flex-col justify-center items-center"
+                        onClick={handleImageFile}
+                    >
                         <div>
                             <Image></Image>
                         </div>
                         <div className="flex flex-row mt-1">
                             <div className="text-amber-700 pr-1">
-                                Upload a file 
+                                Upload a file
                             </div>
                             <div className="text-brown_color">
                                 or drag and drop
                             </div>
                         </div>
                         <div className="text-brown_color font-light flex flex-col 2 mt-1">
-                                PNG, JPG, GIF files are allowed
+                            PNG, JPG, GIF files are allowed
                         </div>
                     </button>
-                )
-                }
+                )}
             </div>
 
             <div className="flex mt-20 mb-10 ml-[610px]">
-                <button 
+                <button
                     onClick={handleCancel}
                     className="whitespace-nowrap py-1 px-4 h-10 w-22 bg-white rounded-md text-gray-950 text-base border-2 border-solid border-gray-300 mr-4 hover:bg-gray-300 hover: duration-300"
-                    >
+                >
                     {"Cancel"}
-                </button>   
-                    
+                </button>
+
                 <button
                     className="whitespace-nowrap py-1 px-4 h-10 w-[80px] bg-teal-800 rounded-xl font-medium text-white text-base hover:bg-gray-800 hover: duration-300"
                     onClick={handleAdd}
-                    >
+                >
                     {"Save"}
                 </button>
-                    
             </div>
         </div>
     );
