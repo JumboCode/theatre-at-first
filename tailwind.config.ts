f9f4c486246ee696560dca779618825de25b406b
oliver bello <obello26@icloud.com>
2024-04-26 16:50:12 -0400

    changed font to montserrat

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+        'sans': 'Montserrat, Arial',
+    },
     extend: {
       colors: {
           selected_tag: '#C8ECE5',
