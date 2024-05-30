91b3401ec364c4d0b36c410086fd917165dcf238
Amitav Nott <58123848+anott03@users.noreply.github.com>
2024-05-30 11:56:58 -0400

    making the header responsive

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -87,6 +87,16 @@ export function User(props) {
     );
 }
 
+// https://feathericons.dev/?search=plus&iconset=feather&format=strict-jsx
+export function Plus(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" className="main-grid-item-icon" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" {...props}>
+      <line x1="12" x2="12" y1="5" y2="19" />
+      <line x1="5" x2="19" y1="12" y2="12" />
+    </svg>
+  );
+}
+
 /* -------------------------    FOOTER ICONS --------------------------------*/
 // https://feathericons.dev/?search=facebook&iconset=feather&format=strict-jsx
 export function Facebook(props) {
